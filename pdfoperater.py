b5382339d72b69983385eb042979c5a7b9b563a7
KeYuZhou <434404896@qq.com>
2022-05-13 16:17:18 +0800

    Update pdfoperater.py

--- pdfoperater.py
+++ pdfoperater.py
@@ -1,6 +1,14 @@
 import xlwt
+import sys
 from pdfminer.high_level import extract_text
-text = extract_text('Single-Stage Monocular 3D Object Detection via Keypoint Estimation.pdf')
+arg=sys.argv
+excelname="reference.xls"
+if(len(arg)==1):
+    print("no pdf name")
+if(len(arg)==3):
+    excelname=arg[2]
+pdfname=arg[1]
+text = extract_text(pdfname)
 txt=text.split("References\n\n")[1]
 txt=txt.replace("\n","")
 refer=txt.split("[")
@@ -9,4 +17,4 @@ Excel = xlwt.Workbook()
 table = Excel.add_sheet('Sheet1', cell_overwrite_ok=True)
 for i in range(len(refer)):
     table.write(i, 0, '['+refer[i])
-Excel.save(r'reference.xls')
+Excel.save(r'excelname')
